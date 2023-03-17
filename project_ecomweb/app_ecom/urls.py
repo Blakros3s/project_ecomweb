90034485c37e13218daa3b38a0413d98d6ee0a24
Blakros3s <blak.ros3s@gmail.com>
2023-03-17 14:09:41 +0545

    displayed add product page, and products on index page, and worked on dashboard

--- project_ecomweb/app_ecom/urls.py
+++ project_ecomweb/app_ecom/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('dashboard/', views.dashboard_index, name='dashboard'),
+
+    # products
+    path('product/add/', views.product_add, name='add-product'),
+    path('products/', views.product_index, name='list-product'),
 ]
