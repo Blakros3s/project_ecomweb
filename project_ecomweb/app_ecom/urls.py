8a3bb1dd91dff0279578f5f233f96729ef31c093
Blakros3s <blak.ros3s@gmail.com>
2023-03-19 13:02:14 +0545

    add category and add products

--- project_ecomweb/app_ecom/urls.py
+++ project_ecomweb/app_ecom/urls.py
@@ -3,6 +3,9 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.dashboard_index, name='dashboard'),
 
+    # categories
+    path('category/add/', views.category_add, name='add-category'),
+    
     # products
     path('product/add/', views.product_add, name='add-product'),
     path('products/', views.product_index, name='list-product'),
